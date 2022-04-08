74bd0524941a0d34762b4cd2cfca2fa930e5dff2
unknown <20PW14@cs2k16.amcs.ws2k12.psgcs.edu>
2022-04-08 12:13:43 +0530

    update 20XW47: ps8 2 completed.

--- Sem4/20XW47 - Mathematical Computing Lab/Matlab/PS-8/ps8.m
+++ Sem4/20XW47 - Mathematical Computing Lab/Matlab/PS-8/ps8.m
@@ -1,5 +1,31 @@
 clear;
 clc;
 
-%1
+%2
 
+% syms x n k;
+% l = pi;
+% a0 = eval(1/l*(int(-k,x,-pi,0) + int(k,x,0,pi)));
+% an = 1/l*(int(-k*cos(n*pi/l*x),x,-pi,0) + int(k*cos(n*pi/l*x),x,0,pi));
+% bn = 1/l*(int(-k*sin(n*pi/l*x),x,-pi,0) + int(k*sin(n*pi/l*x),x,0,pi));
+% 
+% [a0/2,eval(subs(an*cos(n*pi*x/l)+bn*sin(n*pi*x/l),n,[1 2 3 4]))]
+
+% syms n x;
+% f=exp(-x);
+% l=pi;
+% 
+% a0=1/l*int(f,x,0,2*pi);
+% an=1/l*int(f*cos(n*pi*x/l),x,0,2*pi);
+% bn=1/l*int(f*sin(n*pi*x/l),x,0,2*pi);
+% [a0/2,eval(subs(an*cos(n*pi*x/l)+bn*sin(n*pi*x/l),n,[1 2 3 4]))]
+% 
+
+% syms n x;
+% f=(-4*x/pi);
+% l=pi/2;
+% 
+% a0=1/l*(int(f,x,-pi/2,0)+int(-f,x,0,pi/2));
+% an=1/l*(int(f*cos(n*pi/l*x),x,-pi/2,0)+int(-f*cos(n*pi/l*x),x,0,pi/2));
+% bn=1/l*(int(f*sin(n*pi/l*x),x,-pi/2,0)+int(-f*sin(n*pi/l*x),x,0,pi/2));
+% [a0/2,eval(subs(an*cos(n*pi*x/l)+bn*sin(n*pi*x/l),n,[1 2 3 4]))]
