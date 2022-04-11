70a2357cef4cda227572e34d3ef7a3e7ea266c16
20PW14 <76254926+jassuwu@users.noreply.github.com>
2022-04-11 19:04:55 +0530

    20XW46 : started PS11

--- Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/receiver.py
+++ Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/receiver.py
@@ -1,26 +1,24 @@
 #20PW14.receiver.py
 import socket
+
 s = socket.socket()
 host = socket.gethostname()
 port = 65456
 
-s.bind((host,port))
+s.bind((host, port))
 s.listen(5)
 c, addr = s.accept()
 
-arrival = ['0','1','0','1','0','1']
+arrival = ['0', '1', '0', '1', '0', '1']
 recvdArr = []
-
 rn = 0
 
-for i in arrival:
-    message = c.recv(1024).decode()
-    if true:
-        recvdArr.append(message)
-        message = "ACK " + arrival[rn+1]
-        c.send(message.encode())
-        print("Frame[",rn,"]: ", message[-1])
-        rn += 1
-print(rn)
+while (rn < len(arrival)):
+    frame = c.recv(1).decode()
+    print(frame)
+    rn += 1
+    if (rn < len(arrival)):
+        c.send(arrival[rn].encode())
+    else:
+        break
 c.close()
-                                
--- Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/sender.py
+++ Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/sender.py
@@ -1,21 +1,27 @@
 #20PW14.sender.py
 import socket
+
 s = socket.socket()
 host = socket.gethostname()
 port = 65456
-s.connect((host,port))
+s.connect((host, port))
 
 sn = 0
-request = ['0','1','0','1','0','1']
-
-while(sn <= len(request)):
-    print("Sending Frame[",sn,"]...")
-    s.send(request[sn].encode())
-    sn+=1
-    try:
-        message = s.recv(1024).decode()
-        print(message, "received.\n")
-    if request[sn+1] == message[-1]:
+request = ['0', '1', '0', '1', '0', '1']
+canSend = True
+while (sn < len(request)):
+    print(f"sn = {sn}, length of request = {len(request)}")
+    if (canSend):
+        s.send(request[sn].encode())
+        canSend = False
         sn += 1
-
+        if (sn == len(request)):
+            break
+    ACK = s.recv(1024).decode()
+    if ACK != request[sn]:
+        sn -= 1
+        print("Correct ACK not received. Initiating retransmission...")
+    else:
+        canSend = True
+print("Transmission ended.")
 s.close()
