2eae8644a925686a618a00a59e5e3a658f45fce1
Karthickpranav S N <76254926+jassuwu@users.noreply.github.com>
2022-04-13 11:50:03 +0530

    20XW46 : Updated PS11

--- Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/receiver.py
+++ Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/receiver.py
@@ -10,15 +10,16 @@ s.listen(5)
 c, addr = s.accept()
 
 arrival = ['0', '1', '0', '1', '0', '1']
-recvdArr = []
 rn = 0
 
 while (rn < len(arrival)):
     frame = c.recv(1).decode()
     print(frame)
     rn += 1
-    if (rn < len(arrival)):
-        c.send(arrival[rn].encode())
-    else:
-        break
+    if rn != 2:
+        if rn < len(arrival):
+            print(f"Sending ACK {arrival[rn]}...")
+            c.send(arrival[rn].encode())
+        else:
+            break
 c.close()
