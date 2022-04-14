3f829b09f13bd4a0c43dad86813883ad1cf86a08
20PW14 <76254926+karthiiii@users.noreply.github.com>
2022-04-14 15:38:29 +0530

    20XW46 : PS11 Retransmission simulation complete.

--- Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/receiver.py
+++ Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/receiver.py
@@ -10,17 +10,23 @@ s.listen(5)
 c, addr = s.accept()
 
 arrival = ['0', '1', '0', '1', '0', '1']
+arr = []
 rn = 0
 iteration = 0
 while (rn < len(arrival)):
     frame = c.recv(1).decode()
     print(frame)
+    arr.append(frame)
     rn += 1
     iteration += 1
     if rn < len(arrival):
         print(f"Sending ACK {arrival[rn]}...")
         if iteration != 2 and iteration != 5:
             c.send(arrival[rn].encode())
+        else:
+            rn -= 1
+            arr.pop()
     else:
         break
+print("Received : ", arr)
 c.close()
--- Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/sender.py
+++ Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS11 - Stop and Wait ARQ/sender.py
@@ -7,12 +7,12 @@ port = 65456
 s.connect((host, port))
 s.settimeout(1)
 
-
 sn = 0
 request = ['0', '1', '0', '1', '0', '1']
 canSend = True
 while (sn < len(request)):
     if (canSend):
+        print(f"Sending {request[sn]}")
         s.send(request[sn].encode())
         canSend = False
         sn += 1
