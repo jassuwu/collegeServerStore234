65e1a90ded375a519d06593eee7cbe5d49bbf57a
unknown <20PW14@cs2k16.amcs.ws2k12.psgcs.edu>
2022-05-05 12:37:29 +0530

    20XW46 : Completed PS12 httpserver

--- Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS12 - HTTP Server/server.py
+++ Sem4/20XW46 - Computer Networks and TCP-IP Lab/PS12 - HTTP Server/server.py
@@ -1,21 +1,31 @@
 import http.server
+import os
 import socketserver
+from urllib.parse import unquote
+
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def  do_GET(self):
-        #if self.path == '/':
-        #    self.path = 'index.html'
+
+    def do_GET(self):
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
-        
+        path = self.translate_path(self.path)
+        length = int(self.headers['Content-Length'])
+        data = '\n'.join([
+            unquote(x)
+            for x in self.rfile.read(length).decode().split('&')[:-1]
+        ]) + '\n\n\n\n'
+        with open('db.txt', 'a+') as f:
+            f.write(data)
+        self.send_response(301)
+        self.send_header('Location','localhost:65456')
+        self.end_headers()
+        return
 
-# Create an object of the above class
+
 handler_object = MyHttpRequestHandler
-
-PORT = 65456
-my_server = socketserver.TCPServer(("localhost", PORT), handler_object)
-# Star the server
+my_server = socketserver.TCPServer(('localhost', 65456), handler_object)
 my_server.serve_forever()
 
 
